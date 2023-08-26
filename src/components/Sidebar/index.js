816544ad5757c8bc4ffb5fb7bd924caab50bf82f
RileyHeike <rheike@scu.edu>
2023-08-26 13:33:08 -0700

    idek

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -8,20 +8,20 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
-        <Link className='logo' to='/'>
+        <Link className='logo' to='/PersonalWebsite'>
             <img src={LogoRH} alt="logo" />
         </Link>
         <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
+            <NavLink exact="true" activeclassname="active" to="/PersonalWebsite">
                 <FontAwesomeIcon icon={faHome} color = "#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/PersonalWebsite/about">
                 <FontAwesomeIcon icon={faUser} color = "#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/PersonalWebsite/contact">
                 <FontAwesomeIcon icon={faEnvelope} color = "#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
+            <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/PersonalWebsite/portfolio">
                 <FontAwesomeIcon icon={faSuitcase} color = "#4d4d4e" />
             </NavLink>
             
