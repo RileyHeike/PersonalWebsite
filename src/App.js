8b6d70f46d9e3b563389348a773b2f5585a30e42
RileyHeike <rheike@scu.edu>
2023-08-26 12:03:53 -0700

    paths

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/PersonalWebsite" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path='portfolio' element={<Portfolio />} />
+        <Route path="/PersonalWebsite/about" element={<About />} />
+        <Route path="/PersonalWebsite/contact" element={<Contact />} />
+        <Route path='/PersonalWebsite/portfolio' element={<Portfolio />} />
       </Route>
     </Routes>
     </>
