eec9b70bd4710ae59fb49f764f8578601599f159
RileyHeike <rileyheike@gmail.com>
2024-07-06 22:23:56 -0700

    update portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,6 +4,8 @@ import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss";
 import PSF from '../../assets/images/waterdrop.png'
 import Map from '../../assets/images/map.png'
+import Marketplace from '../../assets/images/Marketplace.png'
+import Award from '../../assets/images/First-Place.png'
 
 const Portfolio = () => { 
     const [letterClass, setLetterClass] = useState('text-animate');
@@ -30,6 +32,28 @@ const Portfolio = () => {
                     />
                 </h1>
                 <div className="project-grid">
+                <div className="project-container">
+                        <div className="image">
+                            <img src={ Marketplace }alt="logo"/>
+                        </div>
+                        <div className="text">
+                        <h2 className="project-title">Bronco Swap</h2>
+                            <p className="project-description">
+                            Worked in 4-person team to develop a university marketplace Vue.js app with datatbase interaction using Firebase OAuth and Firestore as a part of <a className="devpost-link" href="https://devpost.com/software/bronco-swap" target="_blank">H4H 2024</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="project-container">
+                        <div className="image">
+                            <img src={ Award }alt="logo"/>
+                        </div>
+                        <div className="text">
+                        <h2 className="project-title">Capital Insights</h2>
+                            <p className="project-description">
+                            Created a Vue.js app that utlizes INRIX APIs, webscraping, and a statistical algorithm to evaluate the quality of storefront property listings in San Francisco. Won 1st place out of 300+ participants at <a className="devpost-link" href="https://devpost.com/software/capital-insights" target="_blank">INRIX Hack 2023</a>
+                            </p>
+                        </div>
+                    </div>
                     <div className="project-container">
                         <div className="image">
                             <img src={ PSF }alt="logo"/>
